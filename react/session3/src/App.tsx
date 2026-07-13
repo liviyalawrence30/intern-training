795c8d5286b3707703f55f9db5e53f9a34bece79
marialiviya <marialiviya.2301149@srec.ac.in>
2026-07-13 17:02:02 +0530

    Complete section 2 - controlled inputs

--- react/session3/src/App.tsx
+++ react/session3/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Counter from './Counter'
+import InternForm from './InternForm'
 import StateTypes from './StateTypes'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
      <>
      <Counter />
      <StateTypes />
+     <InternForm />
 
      </>
   )
