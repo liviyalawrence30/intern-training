aa3a007948575c8bea53ce567f0f1c18f8d6d353
marialiviya <marialiviya.2301149@srec.ac.in>
2026-07-15 16:50:52 +0530

    Complete section 2 - useContext

--- react/session4/src/App.tsx
+++ react/session4/src/App.tsx
@@ -1,10 +1,33 @@
 import './App.css'
-import PropDrillingDemo from './components/PropDrillingDemo'
-function App(){
-  return(
-    <>
-    <PropDrillingDemo />
-    </>
+import Navbar     from './components/Navbar'
+import ThemedCard from './components/ThemedCard'
+import { useInterns } from './contexts/intern-context'
+/*explore section:
+import { useTheme } from './contexts/theme-context'
+function testTheme() {
+  const theme = useTheme()
+  console.log(theme)
+}
+testTheme()
+error: Invalid hook call .Hooks should not be called outside the function component.
+Using them in a normal function violates the rules of hooks.
+*/ 
+function App() {
+  const {interns} = useInterns()
+  return (
+    <div>
+      <Navbar />
+      <div style={{ padding: '16px' }}>
+        
+        {interns.map((intern) => (
+          <ThemedCard
+            key={intern.name}
+            name={intern.name}
+            score={intern.score}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
 export default App
---
+++ react/session4/src/contexts/theme-context.tsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState,type ReactNode } from 'react'
+
+type Theme = 'light' | 'dark'
+
+interface ThemeContextType {
+  theme:       Theme
+  toggleTheme: () => void
+}
+
+const ThemeContext = createContext<ThemeContextType | null>(null)
+/* createContext is initialised with null beacuse it's value comes from the theme provider.
+If it is used outside the provider,it gives the error rather than incorrectly using the default value. */
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState<Theme>('light')
+
+  function toggleTheme(): void {
+    setTheme(prev => prev === 'light' ? 'dark' : 'light')
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export function useTheme(): ThemeContextType {
+  const context = useContext(ThemeContext)
+  if (!context) throw new Error('useTheme must be used inside ThemeProvider')
+  return context
+}
